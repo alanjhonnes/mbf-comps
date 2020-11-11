f265140fa1106b9657188d9c24dd4e8a7edc528d
Ronaldo Zini <rezeuc@gmail.com>
2020-11-11 15:41:29 -0300

    feat: created the bar chart component for liquidity graph

--- projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.stories.ts
+++ projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.stories.ts
@@ -1,15 +1,36 @@
+import { object } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/angular';
 import { BarChartModule } from '../bar-chart.module';
 
 storiesOf('BarChart', module)
     .add('Bar Chart', () => {
+        const chartLabels = ['D0', 'D15', 'D30', 'D60', 'D90', 'D180', 'D361', 'D721', 'D2999'];
+        const chartData = [3000.0, 2000.0, 2500.0, 2000.0, 3000.0, 2500.0, 2000.0, 3000.0, 2500.0];
+        const chartTooltipText = [
+            ['5% da carteira', '01/01/2020'],
+            ['10% da carteira', '01/02/2020'],
+            ['15% da carteira', '01/03/2020'],
+            ['20% da carteira', '01/04/2020'],
+            ['25% da carteira', '01/05/2020'],
+            ['30% da carteira', '01/06/2020'],
+            ['35% da carteira', '01/07/2020'],
+            ['40% da carteira', '01/08/2020'],
+            ['45% da carteira', '01/09/2020']];
+
         return {
             moduleMetadata: {
                 imports: [BarChartModule],
             },
-            props: {},
+            props: {
+                chartLabels: object('chartLabels', chartLabels),
+                chartData: object('chartData', chartData),
+                chartTooltipText: object('chartTooltipText', chartTooltipText)
+            },
             template: `
-                <sf-bar-chart>
+                <sf-bar-chart
+                    [chartLabels]="chartLabels"
+                    [chartData]="chartData"
+                    [chartTooltipText]="chartTooltipText">
                 </sf-bar-chart>
             `,
         };
--- projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.ts
+++ projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, ElementRef, ViewChild } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Chart, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Color, Label } from 'ng2-charts';
 
 @Component({
@@ -10,9 +10,12 @@ import { Color, Label } from 'ng2-charts';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BarChartComponent implements OnInit {
-  @ViewChild("chart") chart: ElementRef;
+  @ViewChild('chart', { static: true }) chart: ElementRef;
 
-  public chartLabels: Label[];
+  @Input() chartLabels: Label[];
+  @Input() chartTooltipText: Array<string[]>;
+  @Input() chartData: number[];
+
   public chartType: ChartType;
   public chartLegend: boolean;
   public chartPlugins: [];
@@ -21,49 +24,173 @@ export class BarChartComponent implements OnInit {
   public chartColors: Color[];
 
   constructor() {
-    this.chartLabels = ['D0', 'D15', 'D30', 'D60', 'D90', 'D180', 'D361', 'D721', 'D2999'];
     this.chartType = 'bar';
     this.chartLegend = false;
     this.chartPlugins = [];
     this.chartColors = [];
-
-    this.chartDataSets = [
-      {
-        data: [3000.0, 2000.0, 2500.0, 2000.0, 3000.0, 2500.0, 2000.0, 3000.0, 2500.0],
-        label: 'R$',
-        hoverBackgroundColor: '#7495B8',
-        backgroundColor: '#7495B8'
-      },
-    ];
-
-    this.chartOptions = {
-      responsive: true,
-      scales: {
-        yAxes: [{
-          stacked: true,
-          ticks: {
-            maxTicksLimit: 6,
-            autoSkip: false,
-          },
-        }]
-      }
-    };
+    this.loadChart();
   }
 
   ngOnInit(): void {
-    // this.loadColors();
+    this.loadChartDataSet();
+    this.loadColors();
+  }
+
+  loadChartDataSet() {
+    this.chartDataSets = [
+      {
+        data: this.chartData,
+        label: 'R$',
+        barThickness: 8,
+      },
+    ];
   }
 
   loadColors() {
-    const gradient = this.chart.nativeElement.getContext('2d').createLinearGradient(0, 0, 0, 400);
+    const el = this.chart.nativeElement.getContext('2d');
+
+    const gradient = el.createLinearGradient(0, 0, 0, 200);
     gradient.addColorStop(0, '#7495B8');
     gradient.addColorStop(1, '#00002D');
 
     this.chartColors = [
       {
-        backgroundColor: gradient
+        borderColor: '#E0E0E0',
+        backgroundColor: '#E0E0E0',
+        hoverBackgroundColor: gradient
       }
     ];
   }
 
+  loadChart() {
+    this.chartOptions = {
+      responsive: true,
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            fontFamily: 'Montserrat',
+          }
+        }],
+        yAxes: [{
+          stacked: true,
+          ticks: {
+            display: false,
+          },
+          gridLines: {
+            lineWidth: 0,
+            zeroLineWidth: 1,
+          },
+        }],
+      },
+      tooltips: {
+        intersect: false,
+        displayColors: false,
+        backgroundColor: '#2A2A5C',
+        xPadding: 8,
+        yPadding: 8,
+        titleFontStyle: '700',
+        titleFontSize: 12,
+        titleSpacing: 14,
+        titleFontFamily: 'Montserrat',
+        bodyFontSize: 12,
+        bodyFontStyle: '400',
+        bodySpacing: 5,
+        bodyFontFamily: 'Montserrat',
+        callbacks: {
+          title: (items) => {
+            return (new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 2
+            })).format(parseFloat(`${items[0].value}`));
+          },
+          label: (item) => {
+            const index = item.index || 0;
+            return this.chartTooltipText[index];
+          }
+        },
+        axis: 'x'
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: false,
+        axis: 'x'
+      },
+    };
+
+    Chart.controllers.bar.prototype.draw = function () {
+      const meta = this.getMeta();
+
+      meta.data.forEach((bar: any) => {
+        const view = bar._view;
+        const ctx = this.chart.ctx;
+        const cornerRadius = 10;
+
+        // Properties of bar.
+        var left, right, top, bottom, radius;
+        left = view.x - view.width / 2;
+        right = view.x + view.width / 2;
+        top = view.y;
+        bottom = view.base;
+
+        ctx.beginPath();
+        ctx.fillStyle = view.backgroundColor;
+        ctx.strokeStyle = view.borderColor;
+
+        // Corner points, init from bottom-left, clockwise.
+        const corners = [
+          [left, bottom],
+          [left, top],
+          [right, top],
+          [right, bottom]
+        ];
+
+        function cornerAt(index: number) {
+          return corners[(index) % 4];
+        }
+
+        // Draw initial rectangle.
+        var corner = cornerAt(0);
+        ctx.moveTo(corner[0], corner[1]);
+
+        for (var i = 1; i < 4; i++) {
+          corner = cornerAt(i);
+
+          var nextCornerId = i + 1;
+          if (nextCornerId == 4) {
+            nextCornerId = 0
+          }
+
+          const width = corners[2][0] - corners[1][0];
+          const height = corners[0][1] - corners[1][1];
+          const x = corners[1][0];
+          const y = corners[1][1];
+
+          radius = cornerRadius;
+
+          // Fix radius being too large.
+          if (radius > height / 2) {
+            radius = height / 2;
+          } if (radius > width / 2) {
+            radius = width / 2;
+          }
+
+          ctx.moveTo(x + radius, y);
+          ctx.lineTo(x + width - radius, y);
+          ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+          ctx.lineTo(x + width, y + height);
+          ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+          ctx.lineTo(x, y + height);
+          ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+          ctx.lineTo(x, y + radius);
+          ctx.quadraticCurveTo(x, y, x + radius, y);
+        }
+
+        ctx.fill();
+      });
+    }
+  }
 }
