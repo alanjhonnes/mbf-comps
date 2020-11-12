a82c8e985dba53836b5d60f1446bc9d281a2a97a
Ronaldo Zini <rezeuc@gmail.com>
2020-11-12 16:36:21 -0300

    feat: refactor chart controller

--- projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.ts
+++ projects/component-library/src/lib/bar-chart/bar-chart/bar-chart.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { Chart, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
+import { Chart, ChartDataSets, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'sf-bar-chart',
@@ -16,57 +16,45 @@ export class BarChartComponent implements OnInit {
   @Input() chartTooltipText: Array<string[]>;
   @Input() chartData: number[];
 
-  public chartType: ChartType;
-  public chartLegend: boolean;
-  public chartPlugins: [];
   public chartDataSets: ChartDataSets[];
   public chartOptions: ChartOptions;
-  public chartColors: Color[];
 
-  constructor() {
-    this.chartType = 'bar';
-    this.chartLegend = false;
-    this.chartPlugins = [];
-    this.chartColors = [];
+  constructor() { }
+
+  ngOnInit(): void {
     this.loadChart();
   }
 
-  ngOnInit(): void {
-    this.loadChartDataSet();
-    this.loadColors();
-  }
-
   ngOnChanges(): void {
-    this.loadChartDataSet();
-  }
-
-  loadChartDataSet() {
-    this.chartDataSets = [
-      {
-        data: this.chartData,
-        barThickness: 8,
-      },
-    ];
-  }
-
-  loadColors() {
-    const ctx = this.chart.nativeElement.getContext('2d');
-    const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-
-    gradient.addColorStop(0, '#7495B8');
-    gradient.addColorStop(1, '#00002D');
-
-    this.chartColors = [
-      {
-        borderColor: '#E0E0E0',
-        backgroundColor: '#E0E0E0',
-        hoverBackgroundColor: gradient
-      }
-    ];
+    this.loadChart();
   }
 
   loadChart() {
+    this.loadChartOptions();
+    this.createChartController();
+
+    const ctx = this.chart.nativeElement.getContext('2d');
+
+    new Chart(ctx, {
+      type: 'barRounded',
+      data: {
+        labels: this.chartLabels,
+        datasets: [{
+          data: this.chartData,
+          barThickness: 8,
+          backgroundColor: '#E0E0E0',
+          hoverBackgroundColor: 'GRADIENT_COLOR'
+        }],
+      },
+      options: this.chartOptions,
+    });
+  }
+
+  loadChartOptions() {
     this.chartOptions = {
+      legend: {
+        display: false
+      },
       responsive: true,
       scales: {
         xAxes: [{
@@ -121,79 +109,92 @@ export class BarChartComponent implements OnInit {
         mode: 'nearest',
         intersect: false,
         axis: 'x'
-      },
+      }
     };
+  }
 
-    Chart.controllers.bar.prototype.draw = function () {
-      const meta = this.getMeta();
+  createChartController() {
+    Chart.defaults.barRounded = Chart.defaults.bar;
+    Chart.controllers.barRounded = Chart.controllers.bar.extend({
+      draw: function () {
+        const meta = this.getMeta();
 
-      meta.data.forEach((bar: any) => {
-        const view = bar._view;
-        const ctx = this.chart.ctx;
-        const cornerRadius = 10;
+        meta.data.forEach((bar: any) => {
+          const view = bar._view;
+          const ctx = this.chart.ctx;
+          const cornerRadius = 10;
 
-        // Properties of bar.
-        var left, right, top, bottom, radius;
-        left = view.x - view.width / 2;
-        right = view.x + view.width / 2;
-        top = view.y;
-        bottom = view.base;
+          // Properties of bar.
+          var left, right, top, bottom, radius;
+          left = view.x - view.width / 2;
+          right = view.x + view.width / 2;
+          top = view.y;
+          bottom = view.base;
 
-        ctx.beginPath();
-        ctx.fillStyle = view.backgroundColor;
-        ctx.strokeStyle = view.borderColor;
+          ctx.beginPath();
 
-        // Corner points, init from bottom-left, clockwise.
-        const corners = [
-          [left, bottom],
-          [left, top],
-          [right, top],
-          [right, bottom]
-        ];
-
-        function cornerAt(index: number) {
-          return corners[(index) % 4];
-        }
-
-        // Draw initial rectangle.
-        var corner = cornerAt(0);
-        ctx.moveTo(corner[0], corner[1]);
-
-        for (var i = 1; i < 4; i++) {
-          corner = cornerAt(i);
-
-          var nextCornerId = i + 1;
-          if (nextCornerId == 4) {
-            nextCornerId = 0
+          // Set color gradient when hovering
+          if (view.backgroundColor == 'GRADIENT_COLOR') {
+            const gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
+            gradient.addColorStop(0, '#7495B8');
+            gradient.addColorStop(1, '#00002D');
+            ctx.fillStyle = gradient;
+          } else {
+            ctx.fillStyle = view.backgroundColor;
           }
 
-          const width = corners[2][0] - corners[1][0];
-          const height = corners[0][1] - corners[1][1];
-          const x = corners[1][0];
-          const y = corners[1][1];
+          // Corner points, init from bottom-left, clockwise.
+          const corners = [
+            [left, bottom],
+            [left, top],
+            [right, top],
+            [right, bottom]
+          ];
 
-          radius = cornerRadius;
-
-          // Fix radius being too large.
-          if (radius > height / 2) {
-            radius = height / 2;
-          } if (radius > width / 2) {
-            radius = width / 2;
+          function cornerAt(index: number) {
+            return corners[(index) % 4];
           }
 
-          ctx.moveTo(x + radius, y);
-          ctx.lineTo(x + width - radius, y);
-          ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-          ctx.lineTo(x + width, y + height);
-          ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-          ctx.lineTo(x, y + height);
-          ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-          ctx.lineTo(x, y + radius);
-          ctx.quadraticCurveTo(x, y, x + radius, y);
-        }
+          // Draw initial rectangle.
+          var corner = cornerAt(0);
+          ctx.moveTo(corner[0], corner[1]);
 
-        ctx.fill();
-      });
-    }
+          for (var i = 1; i < 4; i++) {
+            corner = cornerAt(i);
+
+            var nextCornerId = i + 1;
+            if (nextCornerId == 4) {
+              nextCornerId = 0
+            }
+
+            const width = corners[2][0] - corners[1][0];
+            const height = corners[0][1] - corners[1][1];
+            const x = corners[1][0];
+            const y = corners[1][1];
+
+            radius = cornerRadius;
+
+            // Fix radius being too large.
+            if (radius > height / 2) {
+              radius = height / 2;
+            } if (radius > width / 2) {
+              radius = width / 2;
+            }
+
+            ctx.moveTo(x + radius, y);
+            ctx.lineTo(x + width - radius, y);
+            ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+            ctx.lineTo(x + width, y + height);
+            ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+            ctx.lineTo(x, y + height);
+            ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+            ctx.lineTo(x, y + radius);
+            ctx.quadraticCurveTo(x, y, x + radius, y);
+          }
+
+          ctx.fill();
+        });
+      }
+    });
   }
 }
